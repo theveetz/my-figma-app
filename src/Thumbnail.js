a3da0bf6c9c02ce5ba5067dfb94c23390ac9b130
Nikolina <nikkipersonal2@gmail.com>
2024-07-07 12:37:51 +0930

    Initial commit

---
+++ src/Thumbnail.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './styles.css';
+
+export default function Thumbnail() {
+  return (
+    <div className="thumbnail clip-contents">
+      <div className="group-422">
+        <div className="group-6110">
+          <div className="text">
+            <p className="search-with-seamless-po">
+              Search with Seamless Power
+            </p>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/ozb817lj0rg-2%3A99?alt=media&token=fc75fd35-6582-4a3a-8091-f69bc6ba7d8c"
+              alt="Not Found"
+              className="vector"
+            />
+          </div>
+        </div>
+        <p className="generated-text">
+          AI-driven search is reshaping the digital landscape, offering an
+          ever-evolving and sophisticated approach to information retrieval.
+          With each interaction, these systems learn and adapt, providing users
+          with a personalized and intuitive exploration of vast data landscapes.
+          As AI search technology continues to advance, it not only enhances
+          efficiency but also unlocks new dimensions of user-centric
+          experiences, ensuring that the quest for information is not just a
+          task but a journey of discovery tailored to individual preferences.
+          The future promises even more innovation, making AI search an
+          indispensable tool for navigating the complexities of the digital
+          realm Precision at Every Query: Unleash the power of AI search's
+          accuracy and relevance, ensuring that each search query brings forth
+          precisely what you're looking for. Adaptive Intelligence in Action:
+          Experience a dynamic search journey as AI adapts to your preferences,
+          offering a tailored exploration that evolves with every
+          interaction.offering a tailored exploration that evolves with every
+          interaction.
+        </p>
+      </div>
+      <div className="prompt-bar">
+        <div className="search-bar-2">
+          <div className="search-bar-1">
+            <div className="search-bar">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/ozb817lj0rg-I2%3A66%3B1%3A10?alt=media&token=97be78a2-d9f0-4567-bf72-1becad2a9ba7"
+                alt="Not Found"
+                className="search"
+              />
+              <p className="ask-aquestion">Ask a question</p>
+            </div>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/ozb817lj0rg-I2%3A66%3B1%3A13?alt=media&token=a04353de-c682-4d59-b9a2-30afe1a06478"
+              alt="Not Found"
+              className="send"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
